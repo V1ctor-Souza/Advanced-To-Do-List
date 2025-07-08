390976df5928c5fa339d16a54781e15073f616c1
Victor <victrhugocs0@gmail.com>
2025-07-08 16:25:08 -0300

    dynamic system for creating a simple task after refactoring

--- src/script.js
+++ src/script.js
@@ -1,54 +1,26 @@
 // Criação de tarefa simples e tarefa com subtarefas
 const btnCreateTasks = document.querySelector(".btn-createTasks");
 const mainContainer = document.querySelector(".main-container");
-let firstColumn = document.querySelector(".column:first-child");
+let firstColumn = document.querySelector(".column:first-child")
 
 btnCreateTasks.addEventListener("click", () => {
-    
-    // create element in DOM
-
-        // create elements
-        let taskContainer = document.createElement('article');
-        let labelTask = document.createElement('label');
-        let inputTask = document.createElement('input');
-        let checkmark = document.createElement('span');
-        let taskName = document.createElement('span');
-
-        let management = document.createElement('div');
-        let btnEdit = document.createElement('button');
-        let imgEdit = document.createElement('img');
-        let btnDelete = document.createElement('button');
-        let imgDelete = document.createElement('img');
-    
-
-        // add classlist in elements
-        taskContainer.classList.add("task-container");
-        labelTask.classList.add("task");
-        checkmark.classList.add("checkmark");
-        taskName.classList.add("taskName");
-        management.classList.add("management");
-
-
-        // add elements in DOM
-        firstColumn.appendChild(taskContainer);
-
-        taskContainer.appendChild(labelTask);
-        labelTask.appendChild(inputTask);
-        labelTask.appendChild(checkmark);
-        labelTask.appendChild(taskName);
-
-        taskContainer.appendChild(management);
-        management.appendChild(btnEdit);
-        btnEdit.appendChild(imgEdit);
-        management.appendChild(btnDelete);
-        btnDelete.appendChild(imgDelete);
-
-
-        // add property in elements especific
-        inputTask.type = 'checkbox';
-        taskName.textContent = 'Nome da tarefa';
-        imgEdit.src = "assets/edit.png";
-        imgEdit.alt = "edit task image";
-        imgDelete.src = "assets/delete.png";
-        imgEdit.alt = "delete task image";
+    let taskContainer = createSimpleTask("article", "task-container", undefined, firstColumn);
+    let labelTask = createSimpleTask("label", "task", undefined, taskContainer);
+    let inputTask = createSimpleTask("input", undefined, {type: "checkbox"}, labelTask);
+    let checkmark = createSimpleTask("span", "checkmark", undefined, labelTask);
+    let taskName = createSimpleTask("span", "taskName", {nameTask: "Nome da tarefa"}, labelTask);
 });
+
+// Functions
+
+
+// Function to create simple task
+function createSimpleTask(element, className, attributes = {}, parentElement){
+    let el = document.createElement(element);
+    if (className) el.classList.add(className);
+    for (let attr in attributes){
+        if(attr === 'nameTask') el.textContent = attributes[attr];
+        else el.setAttribute(attr, attributes[attr]);
+    }
+    return parentElement.appendChild(el);
+}
