e00bd0a55182182cb1c7c5679dbf2e5b00104842
Victor <victrhugocs0@gmail.com>
2025-07-10 14:47:57 -0300

    sending main modal

--- src/script.js
+++ src/script.js
@@ -1,23 +1,30 @@
 // Criação de tarefa simples e tarefa com subtarefas
 const btnCreateTasks = document.querySelector(".btn-createTasks");
 const mainContainer = document.querySelector(".main-container");
-let firstColumn = document.querySelector(".column:first-child")
+const mainModal = document.querySelector(".main-modal");
+let firstColumn = document.querySelector(".column:first-child");
 
 btnCreateTasks.addEventListener("click", () => {
-    let taskContainer = createSimpleTask("article", "task-container", undefined, firstColumn);
-    let labelTask = createSimpleTask("label", "task", undefined, taskContainer);
-    let inputTask = createSimpleTask("input", undefined, {type: "checkbox"}, labelTask);
-    let checkmark = createSimpleTask("span", "checkmark", undefined, labelTask);
-    let taskName = createSimpleTask("span", "taskName", {nameTask: "Nome da tarefa"}, labelTask);
+    mainModal.showModal();
+    mainModal.classList.add("animation");
 });
+
+// btnCreateTasks.addEventListener("click", () => {
+//     let taskContainer = createSimpleTask("article", "task-container", undefined, firstColumn);
+//     let labelTask = createSimpleTask("label", "task", undefined, taskContainer);
+//     let inputTask = createSimpleTask("input", undefined, {type: "checkbox"}, labelTask);
+//     let checkmark = createSimpleTask("span", "checkmark", undefined, labelTask);
+//     let taskName = createSimpleTask("span", "taskName", {nameTask: "Nome da tarefa"}, labelTask);
+// });
+
 
 // Functions
 
 
 // Function to create simple task
-function createSimpleTask(element, className, attributes = {}, parentElement){
+function createSimpleTask(element, className, attributes = {}, parentElement, subtask = false){
     let el = document.createElement(element);
-    if (className) el.classList.add(className);
+    if (className) el.classList.add(...className.split(" "));
     for (let attr in attributes){
         if(attr === 'nameTask') el.textContent = attributes[attr];
         else el.setAttribute(attr, attributes[attr]);
